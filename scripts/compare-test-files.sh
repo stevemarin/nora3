4463910a4869dd460263373e2dee04ad9ee6b56d
Steve Marin <steve@PHOENIX-B1C248B.localdomain>
2026-06-01 18:29:35 -0500

    add chapter 11 tests

--- scripts/compare-test-files.sh
+++ scripts/compare-test-files.sh
@@ -3,7 +3,7 @@
 # compares files in local test suite against those in the official test suite
 # which should be cloned into the same directory as this repo
 
-for chapter in $(seq 1 10); do
+for chapter in $(seq 1 11); do
     local_tests="./tests/chapter_$(printf '%02d\n' ${chapter})/"
     original_tests="../writing-a-c-compiler-tests/tests/chapter_${chapter}/"
     diff --recursive --brief ${local_tests} ${original_tests}
